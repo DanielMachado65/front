c769d5dd1fce7ad108271840e57d4cff55319f3e
Daniel Machado <danielmachadopintos@hotmail.com>
2022-07-28 23:03:32 -0300

    feat: add footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-row style="background: gray" class="d-flex justify-center"> 
+      v-
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: "FooterComponent",
+};
+</script>
