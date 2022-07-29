e6c70deaa5cbed80e5b21a485e02a04a932af91e
Daniel Machado <danielmachadopintos@hotmail.com>
2022-07-28 23:03:41 -0300

    feat: app refatoring

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <template>
   <v-app>
     <v-main bg="dark">
-      <v-container fluid>
+      <v-container>
+        <!-- main topics -->
         <Introduction />
         <History />
+
+        <!-- footer  -->
+        <Testimonies />
         <Subscribe />
+        <Footer />
       </v-container>
     </v-main>
   </v-app>
@@ -14,6 +19,8 @@
 import Introduction from "./components/Introduction";
 import History from "./components/History";
 import Subscribe from "./components/Subscribe";
+import Testimonies from "./components/Testimonies";
+import Footer from "./components/Footer.vue";
 
 export default {
   name: "App",
@@ -22,10 +29,8 @@ export default {
     Introduction,
     History,
     Subscribe,
+    Testimonies,
+    Footer,
   },
-
-  data: () => ({
-    //
-  }),
 };
 </script>
---
+++ src/components/Testimonies.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-row>
+    <v-col v-for="item in videos" :key="item">
+      <v-card color="purple darken-2">
+        <v-card-title>{{ item.url }}</v-card-title>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: "TestimoniesComponent",
+
+  data: () => ({
+    videos: [{ url: "asdaksjd" }, { url: "akjsd" }, { url: "asd" }],
+  }),
+};
+</script>
