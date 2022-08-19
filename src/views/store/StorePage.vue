a8563756d94004d297bbde8ca7a68aadf79e6189
Daniel Machado <danielmachadopintos@hotmail.com>
2022-08-18 21:13:29 -0300

    feat: add store page

--- src/views/store/StorePage.vue
+++ src/views/store/StorePage.vue
@@ -1,9 +1,17 @@
 <template>
   <div class="StorePage">
     <h1>Store</h1>
+
+    
   </div>
 </template>
 
 <script>
 export default {};
 </script>
+
+<style scoped>
+.StorePage {
+  height: 90vh;
+}
+</style>
