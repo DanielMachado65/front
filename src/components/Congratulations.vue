a80e91dbef2b41f1a154da52ae2c44a7f95727c5
Daniel Machado <danielmachadopintos@hotmail.com>
2023-02-01 19:41:26 -0300

    feat: congratulations

---
+++ src/components/Congratulations.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-container class="fill-height">
+    <v-row class="d-flex justify-center">
+      <v-col
+        ><p class="text-h2">Parabéns!</p>
+        <p class="text-h3">você vai fazer parte do nosso evento</p>
+        <p class="subtitle-1">
+          aqui está seu pedido <strong>#{{ orderId }}</strong> caso precise de
+          comunicar alguma coisa com nosso time
+        </p>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "CongratulationsComponent",
+  props: {
+    orderId: String,
+  },
+};
+</script>
+
+<style scoped>
+.fill-height {
+  height: 90vh;
+}
+</style>
