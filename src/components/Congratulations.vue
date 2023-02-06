b5be1b0333f601ab632fc9451114cda9f589019a
Daniel Machado <danielmachadopintos@hotmail.com>
2023-02-06 12:54:52 -0300

    feat: add congratulations

--- src/components/Congratulations.vue
+++ src/components/Congratulations.vue
@@ -2,11 +2,15 @@
   <v-container class="fill-height">
     <v-row class="d-flex justify-center">
       <v-col
-        ><p class="text-h2">Parabéns!</p>
-        <p class="text-h3">você vai fazer parte do nosso evento</p>
+        ><p class="text-h2">É OFICIAL!</p>
+        <p class="text-h3">
+          Agora você vai fazer parte dessa experiência. #GOGOBORN
+        </p>
         <p class="subtitle-1">
-          aqui está seu pedido <strong>#{{ orderId }}</strong> caso precise de
-          comunicar alguma coisa com nosso time
+          Aqui está o código do seu pedido (<strong>#{{ orderId }}</strong
+          >) caso precise informar ao nosso time.
+          <br />
+          Logo enviaremos mais informações sobre o evento.
         </p>
       </v-col>
     </v-row>
