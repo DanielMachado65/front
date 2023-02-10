3279a911dc6a7adbc3db3c8b1966549a7cd92c4c
Daniel Machado <danielmachadopintos@hotmail.com>
2023-02-10 10:26:02 -0300

    feat: add component arrows

---
+++ src/components/Arrows.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="d-flex flex-column">
+    <v-icon color="white" size="36" class="mr-2">mdi-chevron-down</v-icon>
+    <v-icon
+      color="white"
+      size="36"
+      class="mr-2"
+      style="margin-top: -25px; margin-bottom: -25px"
+      >mdi-chevron-down</v-icon
+    >
+    <v-icon color="white" size="36" class="mr-2">mdi-chevron-down</v-icon>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ArrowComponent",
+};
+</script>
+
+<style>
+</style>
