3c88f331a034495f6019b4f06474cae9714ed732
Daniel Machado <danielmachadopintos@hotmail.com>
2023-04-18 10:36:34 -0300

    feat: add home page remove img

--- src/views/homepage/HomePage.vue
+++ src/views/homepage/HomePage.vue
@@ -1,27 +1,26 @@
 <template>
   <div>
-    <div v-if="orderId"><Congratulations :order-id="orderId" /></div>
+    <div v-if="orderId">
+      <Congratulations :order-id="orderId"/>
+    </div>
     <div v-else>
-      <Introduction />
+      <Introduction/>
 
       <v-container class="HomePage">
-        <History />
+        <History/>
 
         <!-- footer  -->
-        <Testimonies />
+        <Testimonies/>
       </v-container>
 
       <v-container fluid class="theme--dark mt-12">
-        <About />
+        <About/>
       </v-container>
 
       <v-container fluid class="background-subfoter">
         <v-row>
-          <v-col cols="12" sm="6">
-            <Price />
-          </v-col>
-          <v-col cols="12" sm="6" class="p-5">
-            <subscribe />
+          <v-col cols="12" sm="12" lg="6" class="p-5" style="margin: 0 auto">
+            <subscribe/>
           </v-col>
         </v-row>
       </v-container>
@@ -30,15 +29,8 @@
 </template>
 
 <script>
-import {
-  Introduction,
-  History,
-  Testimonies,
-  Price,
-  About,
-  Subscribe,
-  Congratulations,
-} from "@/components";
+import {About, Congratulations, History, Introduction, Subscribe, Testimonies,} from "@/components";
+
 export default {
   components: {
     Introduction,
@@ -46,7 +38,6 @@ export default {
     Congratulations,
     Testimonies,
     About,
-    Price,
     Subscribe,
   },
   data: () => ({
@@ -60,12 +51,4 @@ export default {
 </script>
 
 <style>
-.background-subfoter {
-  /* color: #1a150b; */
-  /* background-color: #1a150b; */
-}
-
-.title-born {
-  font-family: "LeagueGothic" !important;
-}
 </style>
